6d166beae289235aa53e22b47948bb38043fc365
sberg02 <114252187+sberg02@users.noreply.github.com>
2023-04-09 21:37:13 -0500

    Updated operate function

--- javascript.js
+++ javascript.js
@@ -22,29 +22,36 @@ function divide (x, y) {
 //Operate function that calls operator functions
 function operate (num1, operator, num2) {
     if (operator === '+') {
-        add(num1, num2)
+        add(num1, num2);
     }
     else if (operator === '-') {
+        subtract(num1, num2);
     }
-
+    else if (operator === '*') {
+        multiply(num1, num2);
+    }
+    else if (operator === '/') {
+        divide(num1, num2);
+    }
+    else {
+        return
+    }
 }
 //Button that returns array of all the buttons
 const screenSelect = document.querySelector('.screen');
 const btnSelectAll = document.querySelectorAll('button');
 btnSelectAll.forEach(button => {
     button.addEventListener('click', () => {
-        //Textcontent is just a test. Need to manipulate the array from queryselectall.
-        screenSelect.textContent = button.textContent;
 });
 });
 
 const clearBtn = document.querySelector('#clear-button');
 clearBtn.addEventListener('click', () => {
-    screenSelect.textContent = 0;
+    screenSelect.innerHTML = 0;
 }); 
 
 
 const equalsBtn = document.querySelector('#equals-button');
-clearBtn.addEventListener('click', () => {
-    operate(num1, num2, operator);
+equalsBtn.addEventListener('click', () => {
+    operate(num1, operator, num2);
 }); 
