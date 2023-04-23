3de221d24cc2745c314c08bffa7f1f3f1a5079a9
sberg02 <114252187+sberg02@users.noreply.github.com>
2023-04-23 14:42:15 -0500

    Fixed clear button function. Fixed operate function. Added global variables.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
 //Global variables used in calculations
-let num1;
-let num2;
+let num1 = [];
+let num2 = [];
 let operator;
+let num1Joined;
+let num2Joined;
+let result;
 
 //Functions for each operator
 function add (x, y) {
@@ -21,37 +24,70 @@ function divide (x, y) {
 }
 //Operate function that calls operator functions
 function operate (num1, operator, num2) {
+    let parsed1 = parseInt(num1);
+    let parsed2 = parseInt(num2);
     if (operator === '+') {
-        add(num1, num2);
+        return add(parsed1, parsed2);
     }
     else if (operator === '-') {
-        subtract(num1, num2);
+        return subtract(parsed1, parsed2);
     }
     else if (operator === '*') {
-        multiply(num1, num2);
+        return multiply(parsed1, parsed2);
     }
     else if (operator === '/') {
-        divide(num1, num2);
+        return divide(parsed1, parsed2);
     }
     else {
-        return
+        return 0;
     }
 }
 //Button that returns array of all the buttons
 const screenSelect = document.querySelector('.screen');
 const btnSelectAll = document.querySelectorAll('button');
+const operatorBtn = document.querySelectorAll('.op-btn');
 btnSelectAll.forEach(button => {
-    button.addEventListener('click', () => {
+    button.addEventListener('click', (e) => {
+        let selectNum1 = e.target.textContent;
+        num1.push(selectNum1);
+        num1Joined = num1.join('');
+        screenSelect.textContent = num1Joined;
+        console.log(num1Joined);
 });
 });
-
+//Stores 
+operatorBtn.forEach(button => {
+    button.addEventListener('click', (e) => {
+    operator = e.target.textContent;
+    screenSelect.textContent = operator;
+    console.log(operator);
+    });
+});
+//I think the best route is to store the clicks in the array then use join
+//to mash them into one number. Then you have one array. End array collection 
+//when you hit an operator? Collect second array and join. Then run operate when = is pushed.
 const clearBtn = document.querySelector('#clear-button');
 clearBtn.addEventListener('click', () => {
-    screenSelect.innerHTML = 0;
+    num1 = [];
+    num2 = [];
+    screenSelect.textContent = '';
 }); 
 
 
 const equalsBtn = document.querySelector('#equals-button');
 equalsBtn.addEventListener('click', () => {
-    operate(num1, operator, num2);
+    result = operate(num1Joined, operator, num2Joined);
+    screenSelect.textContent = result;
 }); 
+
+
+/*            if (button == operatorBtn) {
+                screenSelect.innerHTML = '';
+                operator = operatorBtn.textContent;
+                console.log(operator);
+                const selectNum2 = e.target.textContent;
+                num2.push(selectNum2);
+                let num2Joined = num2.join('');
+                screenSelect.textContent = num2Joined;
+                console.log(num2Joined);
+            } */
